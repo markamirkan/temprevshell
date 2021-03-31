83724d43ff18b5b556928029bc7783ea38112e57
Mark Amirkan <61674326+markamirkan@users.noreply.github.com>
2021-03-31 03:37:04 -0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'contact@rococo.media';
+$recipient = 'ayush@rococo.media';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
